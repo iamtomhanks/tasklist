ba7e0cba0ee588323eb19de2ce81d0d84fba3d1d
iamtomhanks <lebrecdesign@gmail.com>
2016-04-13 12:26:17 -0400

    TasklistUpdate1

---
+++ main.js
@@ -0,0 +1,39 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient){
+	Template.tasks.helpers({
+		tasks: function(){
+			return Tasks.find({}, {sort: {createdAt: -1}});
+		}
+	});
+	
+	Template.tasks.events({
+		"submit .add-task": function(event){
+			var name = event.target.name.value;
+			
+			
+			
+			Tasks.insert({
+				name: name,
+				createdAt: new Date(),
+				userId: Meteor.userId()
+			});
+			
+			event.target.name.value = '';
+			
+			return false;
+				
+		},
+		
+		"click .delete-task": function(event){
+			if(confirm('Delete Task?')){
+				Tasks.remove(this._id);	
+			}
+			
+			return false;
+				
+		}
+		
+		
+	});
+}
